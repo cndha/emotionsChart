2eb00119ab3fb29a5d781ab86be271999c42c371
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2022-06-08 01:03:22 -0400

    add placeholder typings

--- src/SunburstChart.tsx
+++ src/SunburstChart.tsx
@@ -1,27 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
+// @ts-ignore
 import * as d3 from "d3";
 import data from "./data.json";
-import { isConstructorDeclaration } from "typescript";
-import HoverModal from './HoverModal';
-import { svg } from "d3";
 
 const SIZE = 975;
 const RADIUS = SIZE / 2;
 
-interface Data {
-  name: string;
-  value?: any;
-}
-
 const SunburstChart = () => {
   const svgRef = React.useRef<SVGSVGElement>(null);
 
-  const partition = (data) =>
+  const partition = (data: any) =>
     d3.partition().size([2 * Math.PI, RADIUS])(
       d3
         .hierarchy(data)
-        .sum((d) => d.value)
-        .sort((a, b) => b.value - a.value) //REMOVE THIS TO NOT SORT?
+        .sum((d: any) => d.value)
+        .sort((a: any, b: any) => b.value - a.value) //REMOVE THIS TO NOT SORT?
     );
 
   const color = d3.scaleOrdinal(
@@ -32,12 +25,12 @@ const SunburstChart = () => {
 
   const arc = d3
     .arc()
-    .startAngle((d) => d.x0)
-    .endAngle((d) => d.x1)
-    .padAngle((d) => Math.min((d.x1 - d.x0) / 2, 0.005))
+    .startAngle((d: any) => d.x0)
+    .endAngle((d: any) => d.x1)
+    .padAngle((d: any) => Math.min((d.x1 - d.x0) / 2, 0.005))
     .padRadius(RADIUS / 2)
-    .innerRadius((d) => d.y0)
-    .outerRadius((d) => d.y1 - 1);
+    .innerRadius((d: any) => d.y0)
+    .outerRadius((d: any) => d.y1 - 1);
 
   const getAutoBox = () => {
     if (!svgRef.current) {
@@ -54,40 +47,43 @@ const SunburstChart = () => {
 
     const svg = d3.select(svgRef.current);
 
-    const tooldiv = d3.select('#chartArea')
-      .append('div')
-      .style('visibility', 'hidden')
-      .style('position', 'absolute')
-      .style('background-color', 'pink')
+    const tooldiv = d3
+      .select("#chartArea")
+      .append("div")
+      .style("visibility", "hidden")
+      .style("position", "absolute")
+      .style("background-color", "pink");
 
     svg
       .append("g")
       .attr("fill-opacity", 0.6)
       .selectAll("path")
-      .data(root.descendants().filter((d) => d.depth))
+      .data(root.descendants().filter((d: any) => d.depth))
       .join("path")
-      .attr("fill", (d) => {
+      .attr("fill", (d: any) => {
         while (d.depth > 1) d = d.parent;
         return color(d.data.name);
       })
       .attr("d", arc)
-      .on('mouseover', (e, d) => {
-        console.log('E:::', e);
-        console.log('D:::', d);
-        tooldiv.style('visibility', 'visible')
-          .text(`${d.data.description}`)
+      .on("mouseover", (e: any, d: any) => {
+        console.log("mouseover::EVENT", e);
+        console.log("mouseover::DATA", d);
+        tooldiv.style("visibility", "visible").text(`${d.data.description}`);
       })
-      .on('mousemove', (e, d) => {
-        tooldiv.style('top', (e.pageY - 50) + 'px')
-          .style('left', (e.pageX - 50) + 'px')
+      .on("mousemove", (e: any, d: any) => {
+        tooldiv
+          .style("top", e.pageY - 50 + "px")
+          .style("left", e.pageX - 50 + "px");
       })
-      .on('mouseout', () => { tooldiv.style('visibility', 'hidden') })
+      .on("mouseout", () => {
+        tooldiv.style("visibility", "hidden");
+      })
       .append("title")
       .text(
-        (d) =>
+        (d: any) =>
           `${d
             .ancestors()
-            .map((d) => d.data.name)
+            .map((d: any) => d.data.name)
             .reverse()
             .join("/")}\n${format(d.value)}`
       );
@@ -102,26 +98,29 @@ const SunburstChart = () => {
       .data(
         root
           .descendants()
-          .filter((d) => d.depth && ((d.y0 + d.y1) / 2) * (d.x1 - d.x0) > 10)
+          .filter(
+            (d: any) => d.depth && ((d.y0 + d.y1) / 2) * (d.x1 - d.x0) > 10
+          )
       )
       .join("text")
-      .attr("transform", function (d) {
+      .attr("transform", function (d: any) {
         const x = (((d.x0 + d.x1) / 2) * 180) / Math.PI;
         const y = (d.y0 + d.y1) / 2;
-        return `rotate(${x - 90
-          }) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
+        return `rotate(${
+          x - 90
+        }) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
       })
       .attr("dy", "0.35em")
-      .text((d) => d.data.name);
+      .text((d: any) => d.data.name);
 
     svg.attr("viewBox", getAutoBox);
-  }, []);
+  }, [arc, color, format]);
 
   return (
     <div id="chartArea">
       <svg width={SIZE} height={SIZE} ref={svgRef} />
     </div>
   );
-}
+};
 
 export default SunburstChart;
